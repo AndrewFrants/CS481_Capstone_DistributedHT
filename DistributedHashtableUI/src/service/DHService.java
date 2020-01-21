109b258a18c106615b1d60443711cac95d0b494a
sharma-palak170 <sharmapalak170@gmail.com>
2020-01-21 12:01:38 -0800

    Adding change node and change entry

--- DistributedHashtableUI/src/service/DHService.java
+++ DistributedHashtableUI/src/service/DHService.java
@@ -152,6 +152,11 @@ public class DHService {
 		node.getTable().copyValuesTo(table);
 	}
 	
+	//removing entry
+	public void removeEntry(String name) 
+	{
+		
+	}
 	/*
 	 * This is a 4 node sample cluster for testing/demo'ing
 	 */
@@ -164,30 +169,30 @@ public class DHService {
 											"Palak Tablet",
 											"Rachana" };
 		
-		String[] keyNames = new String[] { "CS400 - Monday 9-17.pdf\nClick to Download", 
-											"CS400 - Monday 9-24.pdf\nClick to Download",
-											"CS400 - Friday 10-14.pdf\nClick to Download",
-											"CS400 - Wednesday 10-24.pdf\nClick to Download",
-											"CS400 - Monday 11-04.pdf\nClick to Download",
-											"CS400 - Monday 11-14.pdf\nClick to Download",
-											"CS400 - Monday 11-24.pdf\nClick to Download",
-											"CS400 - Friday 12-05.pdf\nClick to Download",
-											"CS411 - Monday 9-17.pdf\nClick to Download", 
-											"CS411 - Wednesday 9-24.pdf\nClick to Download",
-											"CS411 - Monday 10-14.pdf\nClick to Download",
-											"CS411 - Wednesday 10-24.pdf\nClick to Download",
-											"CS411 - Monday 11-04.pdf\nClick to Download",
-											"CS411 - Monday 11-14.pdf\nClick to Download",
-											"CS411 - Monday 11-24.pdf\nClick to Download",
-											"CS411 - Friday 12-05.pdf\nClick to Download",
-											"CS420 - Monday 9-17.pdf\nClick to Download", 
-											"CS420 - Monday 9-24.pdf\nClick to Download",
-											"CS420 - Friday 10-14.pdf\nClick to Download",
-											"CS420 - Monday 10-24.pdf\nClick to Download",
-											"CS420 - Wednesday 11-04.pdf\nClick to Download",
-											"CS420 - Monday 11-14.pdf\nClick to Download",
-											"CS420 - Friday 11-24.pdf\nClick to Download",
-											"CS420 - Monday 12-05.pdf\nClick to Download" 
+		String[] keyNames = new String[] { "CS400 - Monday 9-17.pdf", 
+											"CS400 - Monday 9-24.pdf",
+											"CS400 - Friday 10-14.pdf",
+											"CS400 - Wednesday 10-24.pdf",
+											"CS400 - Monday 11-04.pdf",
+											"CS400 - Monday 11-14.pdf",
+											"CS400 - Monday 11-24.pdf",
+											"CS400 - Friday 12-05.pdf",
+											"CS411 - Monday 9-17.pdf", 
+											"CS411 - Wednesday 9-24.pdf",
+											"CS411 - Monday 10-14.pdf",
+											"CS411 - Wednesday 10-24.pdf",
+											"CS411 - Monday 11-04.pdf",
+											"CS411 - Monday 11-14.pdf",
+											"CS411 - Monday 11-24.pdf",
+											"CS411 - Friday 12-05.pdf",
+											"CS420 - Monday 9-17.pdf", 
+											"CS420 - Monday 9-24.pdf",
+											"CS420 - Friday 10-14.pdf",
+											"CS420 - Monday 10-24.pdf",
+											"CS420 - Wednesday 11-04.pdf",
+											"CS420 - Monday 11-14.pdf",
+											"CS420 - Friday 11-24.pdf",
+											"CS420 - Monday 12-05.pdf" 
 											};
 		int index = 0;
 		
