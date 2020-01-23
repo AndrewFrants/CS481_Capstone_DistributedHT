496dce2f5b00de3cfbe10d193c200c9036aefa1f
sharma-palak170 <sharmapalak170@gmail.com>
2020-01-22 22:27:45 -0800

    fixed bug

--- DistributedHashtableUI/src/frontend/MessageStoreController.java
+++ DistributedHashtableUI/src/frontend/MessageStoreController.java
@@ -53,10 +53,10 @@ public class MessageStoreController {
     }
 
     @GetMapping("/hello-world")
-    @ResponseBody
-    public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="Stranger") String name) {
-    	 return new Greeting(counter.incrementAndGet(), String.format(template, name));
-    }
+    //@ResponseBody
+    //public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="Stranger") String name) {
+    //	 return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    //}
     
     @RequestMapping("/messages/{digest}")
     @ResponseBody
