fb3ae87dad37da4eb2f79ca5b60eac7e984f346b
Dan Adzhiyev <44633623+adzhiyev@users.noreply.github.com>
2020-01-26 16:40:16 -0800

    Added class for routing

---
+++ DistributedHashtableUI/src/service/RoutingTable.java
@@ -0,0 +1,39 @@
+package service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class RoutingTable {
+	int nodeID; // ID of the node that owns the routing table
+	int size; // size of the routing table, which is log(n) of the network size
+	int [] n;  // holds value of node incremented by 2^i
+	ArrayList<ArrayList<Integer>>  searchValues; // list of search values for node n
+	int[] sendToNode; // array of nodes to send request to
+	int successor; // ID of successor node
+	int predecessor; // ID of predecessor node
+	
+	
+	public RoutingTable(int nodeID, int size, ArrayList<Integer> listOfActiveNodes) {
+		
+		this.nodeID = nodeID;
+		this.size = size;
+		n = new int[size];
+		searchValues = new ArrayList<ArrayList<Integer>>();
+		sendToNode = new int[size];
+		setSuccessorNode();
+		setPredecessorNode();
+		
+	}
+	
+	private void setSuccessorNode() {
+
+		
+		
+	}
+	
+	private void setPredecessorNode() {
+		
+	}
+	
+
+}
---
+++ DistributedHashtableUI/src/test/TestNetwork.java
@@ -0,0 +1,13 @@
+package test;
+
+import service.DHService;
+import service.DNode;
+
+public class TestNetwork {
+	
+	public static void main(String[] args) {
+		DHService service = new DHService();
+		DNode node = new DNode("1233.456.34.120");
+	}
+
+}
