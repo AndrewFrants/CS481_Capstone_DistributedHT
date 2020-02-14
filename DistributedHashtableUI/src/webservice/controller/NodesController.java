b4ec5dbf35b6cb45d2c25829ad72b459975e0220
Andy F <andreyf@microsoft.com>
2020-02-13 16:20:53 -0800

    POST nodes is working
    POST nodes is working

--- DistributedHashtableUI/src/webservice/controller/NodesController.java
+++ DistributedHashtableUI/src/webservice/controller/NodesController.java
@@ -12,41 +12,43 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import service.DHService;
+import service.DNode;
 import webservice.DhtWebService;
-import webservice.capstone.entry.*;
+import webservice.entry.*;
 import webservice.entry.Entry;
 
 @RestController
+@RequestMapping("/nodes")
 public class NodesController {
    
+
+	public DHService getWS() {
+		return DhtWebService.DhtService;
+	}
 	
    static {
 	   // initialize mock service
 	   DhtWebService.DhtService = DHService.createFiveNodeCluster(); 
    }
    
-   @RequestMapping(value = "/node/{id}", method = RequestMethod.DELETE)
+   private ResponseEntity<Object> HttpResponse(Object obj) {
+	      return new ResponseEntity<>(obj, HttpStatus.OK);
+   }
+   
+   @RequestMapping(value = "/nodes")
+   public ResponseEntity<Object> getEntry() {
+      return new ResponseEntity<>(getWS().getAllNodes(), HttpStatus.OK);
+   }
+   
+   @RequestMapping(value = "/node/{id}", method = RequestMethod.GET)
    public ResponseEntity<Object> delete(@PathVariable("id") String id) { 
-	   entryRepo.remove(id);
-      return new ResponseEntity<>("Entry is deleted successsfully", HttpStatus.OK);
+	   return HttpResponse(getWS().findNodeByName(id));
    }
    
-   @RequestMapping(value = "/entries/{id}", method = RequestMethod.PUT)
-   public ResponseEntity<Object> updateEntry(@PathVariable("id") String id, @RequestBody Entry entry) { 
-	   entryRepo.remove(id);
-	   entry.setId(id);
-      entryRepo.put(id, entry);
-      return new ResponseEntity<>("Entry is updated successsfully", HttpStatus.OK);
-   }
-   
-   @RequestMapping(value = "/entries", method = RequestMethod.POST)
-   public ResponseEntity<Object> createEntry(@RequestBody Entry entry) {
-	   entryRepo.put(entry.getId(), entry);
+   @RequestMapping(value = "/nodes", method = RequestMethod.POST)
+   public ResponseEntity<Object> createEntry(@RequestBody String newNode) {
+	   getWS().addNode(newNode);
       return new ResponseEntity<>("Entry is created successfully", HttpStatus.CREATED);
    }
    
-   @RequestMapping(value = "/entries")
-   public ResponseEntity<Object> getEntry() {
-      return new ResponseEntity<>(entryRepo.values(), HttpStatus.OK);
-   }
 }
